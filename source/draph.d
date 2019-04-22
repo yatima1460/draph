a9937bfd02d3c354aae68b7397cd48dd15e7eed9
Federico Santamorena <federico@santamorena.me>
2019-04-22 03:39:03 +0200

    boilerplate and token as first argument

--- source/draph.d
+++ source/draph.d
@@ -7,19 +7,41 @@ import std.exception;
 
 immutable auto DRAPH_GRAPH_API_URL = "https://graph.facebook.com/";
 
-string draph_page_publish_text(string page_id, string token, string text)
+
+
+
+string draph_page_publish_text(string token, string object_id, string text)
 {
     enforce(token !is null);
-    enforce(page_id !is null);
+    enforce(object_id !is null);
     enforce(text !is null);
-    immutable auto post_url = DRAPH_GRAPH_API_URL ~ page_id ~ "/feed";
+    immutable auto post_url = DRAPH_GRAPH_API_URL ~ object_id ~ "/feed";
     return post(post_url, ["access_token": token, "message": text]).idup;
 }
-/* TO BE IMPLEMENTED */
-string draph_page_publish_image(string page_id, string token, byte[] image)
+
+
+/***** TO BE IMPLEMENTED *****/
+
+
+string draph_page_publish_image(string token, string page_id, byte[] image)
 {
     enforce(token !is null);
     enforce(page_id !is null);
     enforce(image !is null);
+    return null;
+}
+
+string draph_like(string token, string object_id)
+{
+    return null;
+}
+
+string draph_comment_text(string token, string object_id, string message)
+{
+    return null;
+}
+
+string draph_search(string token, string text)
+{
     return null;
 }
